da27e9927dbcdc7ba8eb050f2b2c456c7cf7cc38
Veljko Drašković <veljkodraskovic82@gmail.com>
2024-10-29 14:18:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
- async function getData() {
-            try {
-                const response = await fetch('https://ipinfo.io/json?token=f26747fef8b2ee');
-                const data = await response.json();
-                document.getElementById("info").textContent = `
-                    Vaša IP adresa: ${data.ip}
-                    Lokacija: ${data.city}, ${data.region}, ${data.country}
-                    Koordinate: ${data.loc}
-                `;
-            } catch (error) {
-                console.error("Error:", error);
-                document.getElementById("info").textContent = "Can't get info.";
-            }
-        }
 
